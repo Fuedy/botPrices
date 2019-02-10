9c424eb5b8b17446c224bde3b395a77d007c3c17
Andre Guimaraes <andrecpg@gmail.com>
2019-02-10 13:12:35 -0200

    Lidando com erros

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from backports import csv
+from selenium.common.exceptions import NoSuchElementException
+import csv
 import io
 import time
 
@@ -10,10 +11,10 @@ import time
 listaVendedores = []
 listaPrecos = []
 
-with io.open("precos" + '.csv', 'a', newline='', encoding='utf-8') as f:
-	writer = csv.writer(f)
+with open('prices.csv', 'w', newline='', encoding='utf-8') as csvfile:
+	writer = csv.writer(csvfile)
 	writer.writerow(["GTIN", "Vendedores", "Precos"])
-	f.close()
+	csvfile.close()
 
 #Define as opções do Chrome
 chrome_options = Options()
@@ -31,32 +32,43 @@ for codigoProduto in codigosProduto:
 
 	browser.get('https://www.google.com.br/search?tbm=shop&hl=pt-BR&q='+ codigoProduto) #Navega até a página pedida
 	time.sleep(1)
-	browser.find_element_by_class_name("sh-dlr__thumbnail").click() #Procura o elemento e clica
-	time.sleep(1)
-	browser.find_element_by_class_name("_-bx").click() #Procura o elemento e clica
+	try:
+		browser.find_element_by_class_name("sh-dlr__thumbnail").click() #Procura o elemento e clica
+		time.sleep(1)
+		browser.find_element_by_partial_link_text("Comparar preços de").click()
+		vendedores = browser.find_element_by_id('os-sellers-table').find_elements_by_class_name('os-seller-name') #Pega todos os nomes de vendedores e coloca num vetor
+		precos = browser.find_element_by_id('os-sellers-table').find_elements_by_class_name('os-price-col') #Pega todos os preços e coloca num vetor
 
-	vendedores = browser.find_element_by_id('os-sellers-table').find_elements_by_class_name('os-seller-name') #Pega todos os nomes de vendedores e coloca num vetor
-	precos = browser.find_element_by_id('os-sellers-table').find_elements_by_class_name('os-price-col') #Pega todos os preços e coloca num vetor
+		#Extrai apenas os textos dos elementos
+		for vendedor in vendedores:
+			listaVendedores.append(vendedor.text)
 
-	#Extrai apenas os textos dos elementos
-	for vendedor in vendedores:
-		listaVendedores.append(vendedor.text)
+		#Extrai apenas os textos dos elementos
+		for preco in precos:
+			listaPrecos.append(preco.text)
 
-	#Extrai apenas os textos dos elementos
-	for preco in precos:
-		listaPrecos.append(preco.text)
+		#Cria o CSV com os vetores de vendedores e preços
+		print(listaVendedores)
+		print(listaPrecos)
 
-	#Cria o CSV com os vetores de vendedores e preços
-	with io.open("precos" + '.csv', 'a', newline='', encoding='utf-8') as f:
-		writer = csv.writer(f)
-		for i in range(0,len(vendedores)):
-			writer.writerow([codigoProduto, listaVendedores[i], listaPrecos[i+1]])
+		with open('prices.csv', 'a', newline='', encoding='utf-8') as csvfile:
+			writer = csv.writer(csvfile)
+			for i in range(0,len(vendedores)):
+				writer.writerow([codigoProduto, listaVendedores[i], listaPrecos[i+1]])
+			csvfile.close()
 
-	listaVendedores = []
-	listaPrecos = []
+		listaVendedores = []
+		listaPrecos = []
 
-	print("Arquivo precos.csv atualizado na pasta")
-	
+		print("Arquivo precos.csv atualizado na pasta")
+
+	except NoSuchElementException as e:
+
+		with open('prices.csv', 'a', newline='', encoding='utf-8') as csvfile:
+			writer = csv.writer(csvfile)
+			for i in range(0,len(vendedores)):
+				writer.writerow(['Erro ao buscar o produto'])
+			csvfile.close()
 
 
 #Fecha o browser
